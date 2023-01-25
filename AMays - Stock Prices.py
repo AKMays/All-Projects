26ef3fc07f4aec7c56b54adbec0e44d572d3c1bc
Adonte Mays <122840119+AKMays@users.noreply.github.com>
2023-01-25 18:41:18 -0500

    Update AMays - Stock Prices.py

--- AMays - Stock Prices.py
+++ AMays - Stock Prices.py
@@ -15,32 +15,18 @@ screen.setup(1000, 1000)
 screen.setworldcoordinates(0, min_price * 0.9, length, max_price * 1.1)
 turtle.speed(0)
 
-adonte = turtle.Turtle()
-yesterday_close = 0
+stock_line = turtle.Turtle()
+previous_price = 0
 x = 0
 
 for today in prices:
-    if yesterday_close < today:
-        adonte.pencolor('green')
-        adonte.goto(x, today)
+    if previous_price < today:
+        stock_line.pencolor('green')
+        stock_line.goto(x, today)
     else:
-        if yesterday_close > today:
-            adonte.pencolor('red')
-            adonte.pensize(yesterday_close - today)
-            adonte.goto(x, today)
-            yesterday_close += 1
-
+        if previous_price > today:
+            stock_line.pencolor('red')
+            stock_line.pensize(previous_price - today)
+            stock_line.goto(x, today)
     x += 1
-    yesterday_close = today
-
-# Iterate through the prices list and plot each price point
-# If the price point is larger than the previous price, the line should be green
-# Otherwise the line should be red
-# HINT:
-#       if statements follow the following syntax:
-#           if x > y:
-#               max = x
-#           else:
-#               max = y
-# BONUS:
-#      Make your lines thicker based on the amount of the price increase or decrease
+    previous_price = today
